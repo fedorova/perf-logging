7f5c54086f29a72f46f9bd4ee3a326b8ae465a76
Sasha Fedorova <sasha@wiredtiger.com>
2016-06-22 15:59:42 -0700

    Script improvement: tracking and modularity

--- WTPERF/run-diff-threads.sh
+++ WTPERF/run-diff-threads.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
-#WT_HOME=/cs/systems/home/fedorova/Work/WiredTiger/wt-fork/build_posix
-WT_HOME=/tmpfs/wt-fork-2544/build_posix
+BRANCH=wt-dev
+WT_HOME=/tmpfs/${BRANCH}/build_posix
 DB_HOME=/tmpfs/WT_TEST/
-OUTPUT_ROOT=/cs/systems/home/fedorova/Work/WiredTiger/WTPERF/EVICTION
+SCRIPT_HOME=${HOME}/Work/WiredTiger/perf-logging/WTPERF
+OUTPUT_ROOT=${HOME}/Work/WiredTiger/WTPERF/EVICTION
+DATE=`date +%Y-%m-%d`
+EVICT_WORKERS=4
 
 #for t in 8 16 48 64 96;
 for t in 16;
 do
-    EXPNAME=WT-2544-QUATCHI-4EV-TIMING-MAY6
+    EXPNAME=${BRANCH}-${EVICT_WORKERS}-EV-${DATE}
     EXPID=${EXPNAME}-${t}T
     OUTPUT=${OUTPUT_ROOT}/TEST-STATS/${EXPID}
     mkdir ${OUTPUT}
 #
-    for i in `seq 1 1`;
+    for i in `seq 1 3`;
     do
 	mkdir ${OUTPUT}/${i}
-	${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ./evict-btree-run.wtperf -o threads=\(\(count=${t},reads=1\)\) 
+	${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ${SCRIPT_HOME}/evict-btree-run.wtperf -o threads=\(\(count=${t},reads=1\)\) -o conn_config=\"cache_size=50M,statistics=\(fast,clear\),statistics_log=\(wait=10\),eviction=\(threads_max=${EVICT_WORKERS}\),eviction=\(threads_min=${EVICT_WORKERS}\)\"
 #
-		mv ${DB_HOME}/test.stat ${OUTPUT}/${i}/.
+	mv ${DB_HOME}/test.stat ${OUTPUT}/${i}/.
+	mv ${DB_HOME}/CONFIG.wtperf ${OUTPUT}/${i}/.
 	mv ${DB_HOME}/WiredTigerStat* ${OUTPUT}/${i}/.
 	python ${WT_HOME}/../tools/wtstats/wtstats.py --clear ${OUTPUT}/${i}/WiredTigerStat*
 	mv wtstats.html ${OUTPUT}/${i}/.
