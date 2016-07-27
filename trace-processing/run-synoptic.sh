7402e49546867767cf9c54c86286fb42b89422c5
Sasha Fedorova <sasha@ece.ubc.ca>
2016-07-27 12:54:00 -0700

    make sure that the synoptic output file has the same major name as the input file

--- trace-processing/run-synoptic.sh
+++ trace-processing/run-synoptic.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-synoptic.sh --dumpInvariants=true --dumpInitialPartitionGraph=true --noRefinement=true --noCoarsening=true --outputCountLabels=true --outputProbLabels=false -c $HOME/Work/Perfume/synoptic/log-args.txt -o ./synoptic-output $@
+synoptic.sh --dumpInvariants=true --dumpInitialPartitionGraph=true --noRefinement=true --noCoarsening=true --outputCountLabels=true --outputProbLabels=false -c $HOME/Work/Perfume/synoptic/log-args.txt -o ./$@  $@
