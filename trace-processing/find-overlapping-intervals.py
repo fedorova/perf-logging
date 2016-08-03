dbbc4dbffc9e033a551ea1277f46772f15c7fe03
Alexandra (Sasha)Fedorova <sasha@ece.ubc.ca>
2016-08-02 17:05:08 -0700

    First draft of changing the script to be able to detect multiple intervals in one processing streak

--- trace-processing/find-overlapping-intervals.py
+++ trace-processing/find-overlapping-intervals.py
@@ -6,14 +6,28 @@ import argparse
 maxStretchLength = 0;
 numFiles = 0;
 timeKeyedRecords = {};
+numFilesForFilter = {};
 
 def parse_file(fname):
 
+    global numFilesForFilter;
     global timeKeyedRecords;
     print "Parsing file " + fname;
 
+    # Find the last component of the file name after the "."
+    # That is the name of the filter applied to the original
+    # trace. Each filter name gets its own dictionary.
+    words = fname.split(".");
+    filter_name = words[len(words)-1];
+
+    if filter_name not in timeKeyedRecords:
+        timeKeyedRecords[filter_name] = {}
+        numFilesForFilter[filter_name] = 0;
+        print "Created dictionary for filter name: " + filter_name;
+
     try:
         logFile = open(fname, "r");
+        numFilesForFilter[filter_name] = numFilesForFilter[filter_name] + 1;
     except:
         print "Could not open file " + fname;
         return;
@@ -29,14 +43,14 @@ def parse_file(fname):
 
         if(words[0] == "-->" or words[0] == "<--"):
             time = long(words[3]);
-            timeKeyedRecords[time] = line;
+            timeKeyedRecords[filter_name][time] = line;
         else:
             continue;
 
 def find_overlapping_intervals():
 
     global maxStretchLength;
-    global numFiles;
+    global numFilesForFilter;
     global timeKeyedRecords;
     stretch = [];
 
@@ -44,27 +58,31 @@ def find_overlapping_intervals():
     # of function enter records (marked with -->) of length
     # numFiles without an intervening function exit record.
     # Every such a sequence is an overlapping interval.
-    for key, record in sorted(timeKeyedRecords.items()):
+    for filter_name, dictionary in timeKeyedRecords.items():
+        print("Processing dictionary " +
+              filter_name + " of " + str(len(dictionary)) + " items.");
 
-        words = record.split(" ");
+        for key, record in sorted(dictionary.items()):
 
-        if(words[0] == "-->"):
-            stretch.append(record);
-        else:
-            if(len(stretch) == numFiles):
-                print("--------------");
-                for i in range(len(stretch)) :
-                    print(stretch[i]);
+            words = record.split(" ");
 
-            if(len(stretch) > maxStretchLength):
-                maxStretchLength = len(stretch);
-            stretch = [];
+            if(words[0] == "-->"):
+                stretch.append(record);
+            else:
+                if(len(stretch) == numFilesForFilter[filter_name]):
+                    print("--------------");
+                    for i in range(len(stretch)) :
+                        print(stretch[i]);
+
+                if(len(stretch) > maxStretchLength):
+                    maxStretchLength = len(stretch);
+
+                stretch = [];
 
 
 def main():
 
     global maxStretchLength;
-    global numFiles;
 
     parser = argparse.ArgumentParser(description=
                                      'Process performance log files');
@@ -76,7 +94,6 @@ def main():
 
     for fname in args.files:
         parse_file(fname);
-        numFiles = numFiles + 1;
 
     find_overlapping_intervals();
     print("Maximium overlapping intervals was " + str(maxStretchLength));
