84379693461dc75966f660e9eb83640e38dd4259
Sasha Fedorova <sasha@wiredtiger.com>
2016-09-01 10:52:14 -0700

    Better script changes to accommodate for the differences between Unix flavors

--- WTPERF/run-diff-threads.sh
+++ WTPERF/run-diff-threads.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 BRANCH=wt-dev-din
-#WT_HOME=${HOME}/Work/WiredTiger/${BRANCH}/build_posix
-WT_HOME=/tmpfs/${BRANCH}/build_posix
+if [ "$OSTYPE" == 'darwin' ]; then
+    WT_HOME=${HOME}/Work/WiredTiger/${BRANCH}/build_posix
+else
+    WT_HOME=/tmpfs/${BRANCH}/build_posix
+fi
 DB_HOME=/tmpfs/WT_TEST/
 SCRIPT_HOME=${HOME}/Work/WiredTiger/perf-logging/WTPERF
 OUTPUT_ROOT=${HOME}/Work/WiredTiger/WTPERF/EVICTION
 DATE=`date +%Y-%m-%d`
-EVICT_WORKERS=4
+EVICT_WORKERS=1
 INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library
 
 #for t in 8 16 48 64 96;
@@ -22,7 +25,11 @@ do
     for i in `seq 1 3`;
     do
 	mkdir ${OUTPUT}/${i}
+if [ "$OSTYPE" == 'darwin' ]; then
 	DYLD_LIBRARY_PATH=${INST_LIB} ${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ${SCRIPT_HOME}/evict-btree-run.wtperf -o threads=\(\(count=${t},reads=1\)\) -o conn_config=\"cache_size=50M,statistics=\(fast,clear\),statistics_log=\(wait=10\),eviction=\(threads_max=${EVICT_WORKERS}\),eviction=\(threads_min=${EVICT_WORKERS}\)\"
+else
+	LD_LIBRARY_PATH=${INST_LIB} ${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ${SCRIPT_HOME}/evict-btree-run.wtperf -o threads=\(\(count=${t},reads=1\)\) -o conn_config=\"cache_size=50M,statistics=\(fast,clear\),statistics_log=\(wait=10\),eviction=\(threads_max=${EVICT_WORKERS}\),eviction=\(threads_min=${EVICT_WORKERS}\)\"
+fi
 #
 	mv ${DB_HOME}/test.stat ${OUTPUT}/${i}/.
 	mv ${DB_HOME}/CONFIG.wtperf ${OUTPUT}/${i}/.
