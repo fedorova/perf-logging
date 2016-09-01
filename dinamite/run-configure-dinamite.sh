c0b29b288d30739c217bfec722dca31b21d883d7
Sasha Fedorova <sasha@wiredtiger.com>
2016-08-31 17:28:01 -0700

    Changes to configure on Linux

--- dinamite/run-configure-dinamite.sh
+++ dinamite/run-configure-dinamite.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library DYLD_LIBRARY_PATH="${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library" LDFLAGS="-L${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library" LIBS="-linstrumentation"  CFLAGS="-O3 -g -Xclang -load -Xclang ${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/Release+Asserts/lib/AccessInstrument.dylib" CC="clang" ../configure --enable-snappy
+if [ "$OSTYPE" == 'darwin' ]; then
+    echo "Configuring for OS X"
+    INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library DYLD_LIBRARY_PATH="${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library" LDFLAGS="-L${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library" LIBS="-linstrumentation"  CFLAGS="-O3 -g -Xclang -load -Xclang ${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/Release+Asserts/lib/AccessInstrument.dylib" CC="clang" ../configure --enable-snappy
+else
+    echo "Configuring for Unix"
+    INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library LD_LIBRARY_PATH="${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library" LDFLAGS="-L${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library" LIBS="-linstrumentation -lpthread"  CFLAGS="-O3 -g -Xclang -load -Xclang ${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/Release+Asserts/lib/AccessInstrument.so" CC="clang" ../configure --enable-snappy
+fi
