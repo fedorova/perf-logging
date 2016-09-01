9b0d9eb11bee933960ee945faacb967e286a51af
Sasha Fedorova <sasha@ece.ubc.ca>
2016-08-31 17:52:17 -0700

    trace-processing/do-all.sh

--- WTPERF/run-diff-threads.sh
+++ WTPERF/run-diff-threads.sh
@@ -5,7 +5,7 @@ DB_HOME=/tmpfs/WT_TEST/
 SCRIPT_HOME=${HOME}/Work/WiredTiger/perf-logging/WTPERF
 OUTPUT_ROOT=${HOME}/Work/WiredTiger/WTPERF/EVICTION
 DATE=`date +%Y-%m-%d`
-EVICT_WORKERS=2
+EVICT_WORKERS=4
 INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library
 
 #for t in 8 16 48 64 96;
@@ -18,7 +18,7 @@ do
     # Grab the map files in case we are using a DINAMITE compilation
     cp ${WT_HOME}/map_* ${OUTPUT}
 #
-    for i in `seq 1 4`;
+    for i in `seq 1 3`;
     do
 	mkdir ${OUTPUT}/${i}
 	DYLD_LIBRARY_PATH=${INST_LIB} ${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ${SCRIPT_HOME}/evict-btree-run.wtperf -o threads=\(\(count=${t},reads=1\)\) -o conn_config=\"cache_size=50M,statistics=\(fast,clear\),statistics_log=\(wait=10\),eviction=\(threads_max=${EVICT_WORKERS}\),eviction=\(threads_min=${EVICT_WORKERS}\)\"
