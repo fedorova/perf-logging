b146e49cb111f4b6eee389a99810499e80fea8bc
Sasha Fedorova <sasha@ece.ubc.ca>
2016-09-01 17:00:10 -0700

    Changes to invoke function argument logging

--- WTPERF/run-diff-threads.sh
+++ WTPERF/run-diff-threads.sh
@@ -9,7 +9,7 @@ DB_HOME=/tmpfs/WT_TEST/
 SCRIPT_HOME=${HOME}/Work/WiredTiger/perf-logging/WTPERF
 OUTPUT_ROOT=${HOME}/Work/WiredTiger/WTPERF/EVICTION
 DATE=`date +%Y-%m-%d`
-EVICT_WORKERS=4
+EVICT_WORKERS=1
 INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library
 
 #for t in 8 16 48 64 96;
@@ -22,7 +22,7 @@ do
     # Grab the map files in case we are using a DINAMITE compilation
     cp ${WT_HOME}/map_* ${OUTPUT}
 #
-    for i in `seq 1 3`;
+    for i in `seq 1 1`;
     do
 	mkdir ${OUTPUT}/${i}
 if [ "$OSTYPE" == 'darwin' ]; then
--- trace-processing/do-all.sh
+++ trace-processing/do-all.sh
@@ -14,8 +14,8 @@ fi
 for file in "$@";
 do
     echo Working on file ${file}
-    ${HOME}/Work/DINAMITE/bintrace-toolkit/trace_parser -p print -m ${BUILD_DIR} ${file} > ${file}.txt
-    ${SCRIPTS_HOME}/process-logs.py ${file}.txt > ${file}.summary.txt
+    ${HOME}/Work/DINAMITE/bintrace-toolkit/trace_parser -p print -a ac_short -m ${BUILD_DIR} ${file} > ${file}.txt
+#    ${SCRIPTS_HOME}/process-logs.py ${file}.txt > ${file}.summary.txt
 #    ${SCRIPTS_HOME}/timing_to_synoptic.py ${file}.txt
 #    ${SCRIPTS_HOME}/run-synoptic.sh ${file}.txt.synoptic
 done
