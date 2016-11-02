17e3b99beb4a5fc13d229bb0918fbc82bdf5498f
Sasha Fedorova <sasha@wiredtiger.com>
2016-11-02 19:28:22 +0000

    Additional test configs/changes

--- WTPERF/run-diff-threads.sh
+++ WTPERF/run-diff-threads.sh
@@ -3,15 +3,18 @@ BRANCH=wt-2898
 if [ "$OSTYPE" == 'darwin' ]; then
     WT_HOME=${HOME}/Work/WiredTiger/${BRANCH}/build_posix
 else
-    WT_HOME=/tmpfs/${BRANCH}/build_posix
+#    WT_HOME=/tmpfs/${BRANCH}/build_posix
+    WT_HOME=${HOME}/Work/WiredTiger/${BRANCH}/build_posix
 fi
-DB_HOME=/tmp/WT_TEST/
+#DB_HOME=/tmp/WT_TEST/
+DB_HOME=/mnt/fast/sasha/WT_TEST/
 SCRIPT_HOME=${HOME}/Work/WiredTiger/perf-logging/WTPERF
 OUTPUT_ROOT=${HOME}/Work/WiredTiger/WTPERF/EVICTION
 DATE=`date +%Y-%b-%d-%H:%M`
-EVICT_WORKERS=20
+EVICT_WORKERS=40
 INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library
-WORKLOAD="evict-btree-stress"
+#WORKLOAD="evict-btree-stress"
+WORKLOAD="500m-btree-populate.wtperf"
 
 #for t in 8 16 48 64 96;
 for t in 4;
@@ -34,6 +37,10 @@ do
 	    fi
 	elif [ "$WORKLOAD" == 'evict-btree-stress' ]; then
 	    ${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ${SCRIPT_HOME}/evict-btree-stress-multi.wtperf  -o conn_config=\"eviction=\(threads_max=${EVICT_WORKERS}\),eviction=\(threads_min=1\)\"
+	elif [ "$WORKLOAD" == '500m-btree-populate.wtperf' ]; then
+	    pushd ${WT_HOME}/bench/wtperf
+	    ./wtperf -h ${DB_HOME} -O ${WT_HOME}/../bench/wtperf/runners/${WORKLOAD} -o conn_config=\"eviction=\(threads_max=${EVICT_WORKERS}\),eviction=\(threads_min=1\)\"
+	    popd
 	fi
 #
 	mv ${DB_HOME}/test.stat ${OUTPUT}/${i}/.
