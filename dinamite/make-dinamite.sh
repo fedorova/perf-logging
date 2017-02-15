3d0ab7497f95d01a9f7ec238e42378a0ed3185fe
Sasha Fedorova <sasha@wiredtiger.com>
2017-02-15 15:22:30 -0800

    Fix error in function filters file. Add the environment variable for map locations in the build script.

--- dinamite/make-dinamite.sh
+++ dinamite/make-dinamite.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ALLOC_IN="${HOME}/Work/WiredTiger/perf-logging/dinamite" DIN_FILTERS="${HOME}/Work/WiredTiger/perf-logging/dinamite/function_filter.json" INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library make -j 4
+BUILD_DIR=`pwd`
+
+echo ${BUILD_DIR}
+
+ALLOC_IN="${HOME}/Work/WiredTiger/perf-logging/dinamite" DIN_FILTERS="${HOME}/Work/WiredTiger/perf-logging/dinamite/function_filter.json" DIN_MAPS="${BUILD_DIR}" INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library make -j 24
 
 
