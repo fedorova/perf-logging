cd78696bdddf60d9f95244a141203754c74c2ead
Sasha Fedorova <sasha.fedorova@mongodb.com>
2019-04-01 17:21:29 -0400

    Small script changes. Automatic iostat collection

--- WTPERF/run-diff-threads.sh
+++ WTPERF/run-diff-threads.sh
@@ -17,7 +17,7 @@ INST_LIB=${HOME}/Work/DINAMITE/LLVM/llvm-3.5.0.src/projects/dinamite/library
 ENABLE_OPTRACK=false
 #PERF="perf stat -e 'syscalls:sys_enter_*'"
 #PERF="perf record -e sched:sched_stat_sleep -e sched:sched_switch -e sched:sched_process_exit -a -g -o perf.data.raw"
-PERF="perf record -g"
+#PERF="perf record -g"
 #PERF=""
 
 #WORKLOAD="500m-btree-50r50u.wtperf"
@@ -31,14 +31,17 @@ PERF="perf record -g"
 #WORKLOAD="evict-btree-stress.wtperf"
 #WORKLOAD="evict-btree-stress-multi-run.wtperf"
 #WORKLOAD="evict-lsm-readonly.wtperf"
-WORKLOAD="many-table-stress.wtperf"
+#WORKLOAD="lsm-read.wtperf"
+#WORKLOAD="lsm-populate.wtperf"
+#WORKLOAD="lsm-update.wtperf"
+#WORKLOAD="many-table-stress.wtperf"
 #WORKLOAD="medium-btree.wtperf"
 #WORKLOAD="medium-multi-btree-log-run.wtperf"
 #WORKLOAD="medium-multi-lsm-run.wtperf"
 #WORKLOAD="mongodb-secondary-apply-run.wtperf"
 #WORKLOAD="multi-btree-read-heavy-stress-run.wtperf"
 #WORKLOAD="multi-btree-zipfian-workload.wtperf"
-#WORKLOAD="small-btree-run.wtperf"
+WORKLOAD="small-btree-run.wtperf"
 
 #DINAMITE_TRACE_DIR="/mnt/fast/sasha"
 DINAMITE_TRACE_DIR="/dev/shm"
@@ -47,6 +50,8 @@ DINAMITE_TRACE_DIR="/dev/shm"
 #DINAMITE_TRACE_DIR="/tmp"
 #EXCLUDE_TID=""
 #NAME="dinamite"
+
+IOSTAT_PID=""
 
 #for t in 8 16 48 64 96;
 for t in 'default';
@@ -67,6 +72,9 @@ do
 	mkdir ${OUTPUT}/${i}
 	OPTRACK_DIR=${OUTPUT}/${i}
 	pushd ${WT_HOME}/bench/wtperf
+	LC_TIME=en_US.UTF-8 iostat -k -t -x 1 > ${OUTPUT}/${i}/iostat.log &
+	IOSTAT_PID=$!
+
 	if [ "$OSTYPE" == 'darwin' ]; then
 	    DINAMITE_TRACE_PREFIX=${DINAMITE_TRACE_DIR} DYLD_LIBRARY_PATH=${INST_LIB} WIREDTIGER_OPTRACK=${HOME}/Work/WiredTiger/WTPERF ${WT_HOME}/bench/wtperf/wtperf -h ${DB_HOME} -O ${SCRIPT_HOME}/${WORKLOAD} -o conn_config=\"statistics=\(fast\),statistics_log=\(wait=1\),operation_tracking=\(enabled=${ENABLE_OPTRACK},path=${OPTRACK_DIR}\)\"
 	else
@@ -76,6 +84,8 @@ do
 	    fi
 	fi
 	popd
+
+	kill ${IOSTAT_PID}
 
 	mv ${DB_HOME}/test.stat ${OUTPUT}/${i}/.
 	mv ${DB_HOME}/CONFIG.wtperf ${OUTPUT}/${i}/.
--- WTPERF/small-btree-create.sh
+++ WTPERF/small-btree-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BRANCH=wt-3190
+BRANCH=wt-dev
 if [ "$OSTYPE" == 'darwin' ]; then
     WT_HOME=${HOME}/Work/WiredTiger/${BRANCH}/build_posix
 else
@@ -7,8 +7,8 @@ else
 #    WT_HOME=/tmpfs/${BRANCH}/build_posix
 fi
 #DB_HOME=/tmp/WT_TEST
-#DB_HOME=/mnt/fast/sasha/WT_TEST/
-DB_HOME=$HOME/Work/WiredTiger/WT_TEST/
+DB_HOME=/mnt/data0/sasha/WT_TEST/
+#DB_HOME=$HOME/Work/WiredTiger/WT_TEST/
 SCRIPTS_HOME=$HOME/Work/WiredTiger/perf-logging/WTPERF
 
 mkdir ${DB_HOME}
