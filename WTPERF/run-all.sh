d1b873bca283c7acc8972e44d59cf282c8ca8474
Sasha Fedorova <sasha@ece.ubc.ca>
2020-11-23 11:54:01 -0800

    Script changes

--- WTPERF/run-all.sh
+++ WTPERF/run-all.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-EXP_TAG="BASELINE"
+ulimit -c unlimited
+
+EXP_KIND="NVRAM"
+MEMORY_LIMIT_GB=32
+CACHE_SIZE_LIMIT_GB=`expr ${MEMORY_LIMIT_GB} - 4`
+EXP_TAG=${MEMORY_LIMIT_GB}GB-${EXP_KIND}
 POSTFIX=""
-#COMMAND_PREFIX="cgexec -g memory:8G "
 
-# Drop caches
+# Create a huge ramdisk file to limit the size of
+# available DRAM. The amount of DRAM on howesound is about 188GB.
+# To limit the amount of RAM to 16GB, I create a 172GB file on ramdisk
 #
-#echo 3 > /proc/sys/vm/drop_caches
-
-free -h
+let block_count="(188 - ${MEMORY_LIMIT_GB}) * 1024"
+echo "Creating a file on ramdisk with ${block_count} 1MB blocks"
+dd < /dev/zero bs=1048576 count=${block_count} > /mnt/ramdisk/sasha/testfile
+ls -lh /mnt/ramdisk/sasha
 
 TEST_BRANCH=wt-6022
 #ORIG_BRANCH=wt-dev-morecache
@@ -16,21 +23,42 @@ TEST_BRANCH=wt-6022
 # For situation when I want to run as root, but
 # have the output land in my home directory, set HOME explicitly
 #
-HOME=/home/sasha
+if [[ "$OSTYPE" == *"linux"* ]]; then
+    HOME=/home/sasha
+fi
 
-export WIREDTIGER_CONFIG="statistics=(all)"
-#export WIREDTIGER_CONFIG="statistics=(all),statistics_log=(sources=(\"file:\"))"
+if [[ "$EXP_KIND" == *"DRAM"* ]]; then
+    WIREDTIGER_BASE_CONFIG="statistics=(all)"
+elif [[ "$EXP_KIND" == *"NVRAM"* ]]; then
+    WIREDTIGER_BASE_CONFIG="statistics=(all),block_cache=[enabled=true,size=230GB,type=nvram,path=/mnt/pmem/sasha,hashsize=32768]"
+fi
+
+echo "Base config for $EXP_KIND experiment: $WIREDTIGER_BASE_CONFIG"
+
+export MEMKIND_HOG_MEMORY=1
+#export WIREDTIGER_BASE_CONFIG="statistics=(all),statistics_log=(sources=(\"file:\"))"
+env
 
 # We run this one explicitly before relevant workload benchmarks
 #500m-btree-populate.wtperf
 
-# These don't produce any interesting numbers
+# These don't produce any interesting numbers. Some of them throttle operations.
+#checkpoint-latency-0.wtperf${POSTFIX}
+#checkpoint-stress.wtperf${POSTFIX}
 #mongodb-large-oplog.wtperf
 #mongodb-oplog.wtperf
 #mongodb-secondary-apply.wtperf
 #mongodb-small-oplog.wtperf
 #truncate-btree-populate.wtperf
 #truncate-btree-workload.wtperf
+
+# Invalid configuration value
+#checkpoint-latency-1.wtperf${POSTFIX}
+
+# Fail with errors.
+#metadata-split-test.wtperf${POSTFIX}
+#multi-btree-stress.wtperf${POSTFIX}
+#multi-btree.wtperf${POSTFIX}
 
 # This one is VERY long and uses little memory
 #multi-btree-long.wtperf${POSTFIX}
@@ -42,78 +70,19 @@ export WIREDTIGER_CONFIG="statistics=(all)"
 # This one fails with "Too many open files" error
 #many-table-stress.wtperf${POSTFIX}
 
+# Include only the workloads whose total disk and
+# cache size exceeds 16GB
+#
 TEST_WORKLOADS="
 500m-btree-50r50u.wtperf${POSTFIX}
 500m-btree-80r20u.wtperf${POSTFIX}
-500m-btree-rdonly.wtperf${POSTFIX}
-checkpoint-latency-0.wtperf${POSTFIX}
-checkpoint-latency-1.wtperf${POSTFIX}
-checkpoint-schema-race.wtperf${POSTFIX}
-checkpoint-stress-schema-ops.wtperf${POSTFIX}
-checkpoint-stress.wtperf${POSTFIX}
-evict-btree-1.wtperf${POSTFIX}
-evict-btree-readonly.wtperf${POSTFIX}
 evict-btree-scan.wtperf${POSTFIX}
-evict-btree-stress-multi.wtperf${POSTFIX}
-evict-btree-stress.wtperf${POSTFIX}
-evict-btree.wtperf${POSTFIX}
-evict-fairness.wtperf${POSTFIX}
-evict-lsm-1.wtperf${POSTFIX}
-evict-lsm-readonly.wtperf${POSTFIX}
-evict-lsm.wtperf${POSTFIX}
-index-pareto-btree.wtperf${POSTFIX}
-insert-rmw.wtperf${POSTFIX}
 large-lsm.wtperf${POSTFIX}
-log.wtperf${POSTFIX}
-long-txn-btree.wtperf${POSTFIX}
-long-txn-lsm.wtperf${POSTFIX}
-medium-btree.wtperf${POSTFIX}
-medium-lsm-async.wtperf${POSTFIX}
-medium-lsm-compact.wtperf${POSTFIX}
-medium-lsm.wtperf${POSTFIX}
-medium-multi-btree-log-partial.wtperf${POSTFIX}
-medium-multi-btree-log.wtperf${POSTFIX}
-medium-multi-lsm-noprefix.wtperf${POSTFIX}
-medium-multi-lsm.wtperf${POSTFIX}
-metadata-split-test.wtperf${POSTFIX}
-modify-force-update-large-record-btree.wtperf${POSTFIX}
-modify-large-record-btree.wtperf${POSTFIX}
-multi-btree-read-heavy-stress.wtperf${POSTFIX}
-multi-btree-stress.wtperf${POSTFIX}
-multi-btree-zipfian-workload.wtperf${POSTFIX}
-multi-btree.wtperf${POSTFIX}
-overflow-10k.wtperf${POSTFIX}
-overflow-130k.wtperf${POSTFIX}
-parallel-pop-btree.wtperf${POSTFIX}
-parallel-pop-lsm.wtperf${POSTFIX}
-parallel-pop-stress.wtperf${POSTFIX}
-small-btree.wtperf${POSTFIX}
-small-lsm.wtperf${POSTFIX}
-update-btree.wtperf${POSTFIX}
-update-checkpoint-btree.wtperf${POSTFIX}
-update-checkpoint-lsm.wtperf${POSTFIX}
-update-delta-mix1.wtperf${POSTFIX}
-update-delta-mix2.wtperf${POSTFIX}
-update-delta-mix3.wtperf${POSTFIX}
-update-grow-stress.wtperf${POSTFIX}
-update-large-lsm.wtperf${POSTFIX}
-update-large-record-btree.wtperf${POSTFIX}
-update-lsm.wtperf${POSTFIX}
-update-only-btree.wtperf${POSTFIX}
-update-shrink-stress.wtperf${POSTFIX}"
-
-# Run these later
-TEST_WORKLOADS="
-checkpoint-latency-1.wtperf${POSTFIX}"
-
-#TEST_WORKLOADS="
-#evict-btree.wtperf${POSTFIX}
-#evict-btree-readonly.wtperf${POSTFIX}"
+update-large-lsm.wtperf${POSTFIX}"
 
 if [[ "$OSTYPE" == *"darwin"* ]]; then
     TEST_BASE=${HOME}/Work/WiredTiger/WTPERF
 else
-#    TEST_BASE=/mnt/pmem/sasha
     TEST_BASE=/mnt/ssd/sasha
 fi
 
@@ -142,23 +111,47 @@ do
     fi
 done
 
+SCRIPT_HOME=$(pwd)
+
 # Run the workloads
 #
-
 for workload in ${TEST_WORKLOADS};
 do
     #    for branch in ${TEST_BRANCH} ${ORIG_BRANCH};
     for branch in ${TEST_BRANCH};
     do
         # Run the test workload
-        DB_HOME=${TEST_BASE}/WT_TEST1
+        DB_HOME=${TEST_BASE}/WT_TEST
 
         echo ${workload} ${branch}
 
         cd ${HOME}/Work/WiredTiger/${branch}/build_posix/bench/wtperf
 
+	unset WIREDTIGER_CONFIG
+	export WIREDTIGER_CONFIG=${WIREDTIGER_BASE_CONFIG}
+
+	# If we are memory-limiting, adjust the cache size to
+	# correspond to the memory limit
+	#
+	${SCRIPT_HOME}/extract-cache-size.py ../../../bench/wtperf/runners/${workload}
+	cache_size=$?
+
+	echo Original cache size: $cache_size GB
+	if [ "$cache_size" -gt "${CACHE_SIZE_LIMIT_GB}" ]; then
+	    echo Changing to ${CACHE_SIZE_LIMIT_GB} GB
+	    export WIREDTIGER_CONFIG=${WIREDTIGER_CONFIG},"cache_size=${CACHE_SIZE_LIMIT_GB}G"
+	else
+	    echo "Leaving cache size unchanged..."
+	fi
+	echo $WIREDTIGER_CONFIG
+
         for iter in {1..2};
         do
+	    # Drop caches
+	    #
+	    echo 3 > /proc/sys/vm/drop_caches
+	    free -h
+
             rm -rf ${DB_HOME}/*
             echo Iteration ${iter}
 	    #
@@ -180,7 +173,12 @@ do
 	    #
             ${COMMAND_PREFIX} ./wtperf -h ${DB_HOME} -O ../../../bench/wtperf/runners/${workload}
 
+	    pid=$!
+
 	    # Save the configuration
+	    echo $pid > ${OUTPUT_BASE}/${branch}/${workload}.${pid}.${iter}
+	    cp ${DB_HOME}/*core* ${OUTPUT_BASE}/${branch}/${workload}.${pid}.${iter}.core
+	    cp /var/crash/*core* ${OUTPUT_BASE}/${branch}/${workload}.${pid}.${iter}.core-varcrash
 	    cp ${DB_HOME}/CONFIG.wtperf ${OUTPUT_BASE}/${branch}/${workload}.config.${iter}
 	    cat ${DB_HOME}/WiredTiger.basecfg >> ${OUTPUT_BASE}/${branch}/${workload}.config.${iter}
 	    # Save the amount of disk space used by the database
