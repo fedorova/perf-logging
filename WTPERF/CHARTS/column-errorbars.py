b0a8d012a4b546a08523c9a077e02397392099f1
Alexandra (Sasha)Fedorova <sasha@ece.ubc.ca>
2021-10-27 11:20:45 -0700

    Generate charts with a desired sequential colormap.

--- WTPERF/CHARTS/column-errorbars.py
+++ WTPERF/CHARTS/column-errorbars.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from cycler import cycler
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -72,29 +73,39 @@ def normalizeBy(baselineDFName):
     for name, df in dataFrames.items():
         df['throughput'] = df['throughput']/baselineThroughput;
 
-def example():
+#
+# Build charts using all color maps, cycling through their colors.
+#
+def buildChartsFromColormaps(df, yNames, errNames, chartName, colormaps, reverseColors):
 
-    df = pd.DataFrame()
+    for cmapName in colormaps:
+        n = len(yNames);
+        colors = [plt.get_cmap(cmapName)(1. * i/n) for i in range(n)];
+        if (reverseColors):
+            colors.reverse();
 
-    #the groups can vary 
-    grp1 = 'MM-16GB'
-    grp2 = 'MM-32GB'
-    grp3 = 'NVCache-16GB'
+        print(cmapName);
+        plt.rc('axes', prop_cycle=cycler('color', colors));
 
-    df['label'] = ['YCSB-A.read','YCSB-A.update','YCSB-C.read']
-    df[grp1+'_int'] = [1, 1, 1]
-    df[grp1+'_SD'] = [0.02, 0.01, 0.02]
-    df[grp2+'_int'] = [1.05, 1.06, 1.26]
-    df[grp2+'_SD'] = [0.02, 0.01, 0.02]
-    df[grp3+'_int'] = [1.01, 1.21, 1.48]
-    df[grp3+'_SD'] = [0, 0, 0.02]
+        ax = df.plot.bar(x=chartName, y=yNames, yerr=df[errNames].T.values);
+        #ax.set_prop_cycle(cycler('color', colors));
+        ax.set_ylabel('Normalized throughput');
 
-    ax = df.plot.bar(x='label', 
-                     y=[grp1+'_int',grp2+'_int',grp3+'_int'],
-                     yerr=df[[grp1+'_SD',grp2+'_SD',grp3+'_SD']].T.values)
-    print(df)
-    ax = df.plot.bar(x='label', y=[grp1+'_int',grp2+'_int',grp3+'_int'], yerr=df[[grp1+'_SD', grp2+'_SD', grp3+'_SD']].values)
-    plt.show();
+        # Place a legend above this subplot, expanding itself to
+        # fully use the given bounding box.
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
+               ncol=3, mode="expand", borderaxespad=0., frameon=False);
+        # Place a legend to the right of this smaller subplot.
+        # plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+        plt.tight_layout();
+
+        # If we are using only one colormap, omit the colormap name from
+        # the figure title.
+        #
+        if (len(colormaps) == 1):
+            plt.savefig(chartName + ".png");
+        else:
+            plt.savefig(chartName + "=" + str(cmapName) + ".png");
 
 def generateChart(baseline, chartName):
 
@@ -102,15 +113,30 @@ def generateChart(baseline, chartName):
 
     df = pd.DataFrame();
 
+    #
+    # The baseline dataframe is a multi-index, with data grouped by
+    # workload and operation. Workload and operation are part of the
+    # index, but we want to generate x-axis labels from them.
+    # So we convert the original dataframe into a flat one, where
+    # workload and operations each get their proper column.
+    #
     baseDF = dataFrames[baseline];
     flattenedDF = pd.DataFrame(baseDF.to_records());
     flattenedDF['workload'] = flattenedDF['workload'].str.upper();
-    print(flattenedDF);
 
-
+    #
+    # This is the new dataframe that will be plotted. We
+    # begin by creating a column that contains a concatenation
+    # of the workload name and the operation type.
+    #
     df[chartName] = flattenedDF['workload'].map(str) + "." + \
                   flattenedDF['operation'].map(str);
 
+    #
+    # We gather the throughput and relative standard deviation from
+    # all the data frames we want to display and append them to the
+    # output dataframe.
+    #
     yNames = [];
     errNames = [];
     for name, data in dataFrames.items():
@@ -121,10 +147,17 @@ def generateChart(baseline, chartName):
 
     print(df);
 
-    ax = df.plot.bar(x=chartName, y=yNames, yerr=df[errNames].T.values)
-    ax.set_ylabel('Normalized throughput');
-    plt.tight_layout();
-    plt.savefig(chartName + ".png");
+    #
+    # Sequential colormaps I like:
+    #
+    # PiYG (reversed), PRGn (reversed), PuOr_r (reversed), PuRd_r (reversed),
+    # RdBu (reversed), RdBu_r (reversed), Spectral (reversed)
+    # BrBg, BuPu.
+    #
+    # buildChartsFromColormaps(df, yNames, errNames, chartName, plt.colormaps());
+    #
+    colormaps = ['PRGn'];
+    buildChartsFromColormaps(df, yNames, errNames, chartName, colormaps, True);
 
 def main():
 
