bedff609f527583f9dadcc1845e22502bbb657be
Sasha Fedorova <sasha.fedorova@10gen.com>
2023-11-21 14:21:16 -0800

    Get a CSV input file for BTree visualization

---
+++ trace-processing/convert-csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Read CSV file
+input_file_path = 'evict-btree-trace.csv'  # Replace 'your_input_file.csv' with the actual filename
+df = pd.read_csv(input_file_path)
+
+# Select the 2nd and 6th columns
+selected_columns = df.iloc[:, [1, 5]]  # Assuming columns are zero-indexed
+
+# Write the result to a new CSV file
+output_file_path = 'output_file.csv'  # Replace 'output_file.csv' with the desired output filename
+selected_columns.to_csv(output_file_path, index=False)
