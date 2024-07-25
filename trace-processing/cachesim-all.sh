e8923725a7bf700d77eac89ab25048bd9d2e00c4
Sasha Fedorova <sasha.fedorova@mongodb.com>
2024-07-25 15:37:35 -0700

    Added lrb

--- trace-processing/cachesim-all.sh
+++ trace-processing/cachesim-all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 # List of algorithms
-algorithms=("2Q" "ARC" "ARCv0" "Belady" "Cacheus" "Clock" "FIFO" "FIFOMERGE" "FIFO-REINSERTION" "GDSH" "Hyperbolic" "LECAR" "LECARv0" "LIRS" "LRU" "LFU" "LHD" "SIZE" "SLRU" "SLRUv0" "TinyFLU")
+algorithms=("2Q" "ARC" "ARCv0" "Belady" "Cacheus" "Clock" "FIFO" "FIFOMERGE" "FIFO-REINSERTION" "GDSH" "Hyperbolic" "LECAR" "LECARv0" "LIRS" "LRB" "LRU" "LFU" "LHD" "SIZE" "SLRU" "SLRUv0" "TinyFLU")
 
 for algorithm in "${algorithms[@]}"
 do
-	./bin/cachesim /data/sasha/TRACES/ycsb-c-13GB-2GBcache-6min-traceBeforeEvict.cachesim.generic csv "$algorithm" 2259MB -t "time-col=1, obj-id-col=2, obj-size-col=3" > "$algorithm.out"
+	./bin/cachesim /data/sasha/TRACES/ycsb-c-13GBdb-4GBcache-6min-Yuri-fix.generic csv "$algorithm" 2259MB -t "time-col=1, obj-id-col=2, obj-size-col=3" > "$algorithm.out"
 	grep 'throughput' "$algorithm.out" | grep -o 'miss ratio [^,]*' | awk -v alg="$algorithm" '{print alg, $3}'
 done
 
